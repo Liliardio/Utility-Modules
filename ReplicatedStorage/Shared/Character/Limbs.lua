287c335723f4985a48596b4383cda98a86f2f4d0
Liliardio <70353960+Liliardio@users.noreply.github.com>
2025-08-05 16:37:22 -0400

    Updated ReplicatedStorage/Shared/Character/Limbs.lua

---
+++ ReplicatedStorage/Shared/Character/Limbs.lua
@@ -0,0 +1,109 @@
+-- @ScriptType: ModuleScript
+
+return {
+	RightLeg = {
+		'RightUpperLeg';
+		'RightLowerLeg';
+		'RightFoot';
+		'Right Leg';
+	};
+	LeftLeg = {
+		'LeftUpperLeg';
+		'LeftLowerLeg';
+		'LeftFoot';
+		'Left Leg';
+	};
+	RightArm = {
+		'RightUpperArm';
+		'RightLowerArm';
+		'RightHand';
+		'Right Arm';
+	};
+	LeftArm = {
+		'LeftUpperArm';
+		'LeftLowerArm';
+		'LeftHand';
+		'Left Arm';
+	};
+	Torso = {
+		'LowerTorso';
+		'UpperTorso';
+		'Torso';
+	};
+	Head = {
+		'Head'
+	};
+	UpperBody = {
+		'UpperTorso';
+		'Torso';
+		'LowerTorso';
+		'RightUpperLeg';
+		'LeftUpperLeg';
+		'LeftUpperArm';
+		'LeftLowerArm';
+		'LeftHand';
+		'Left Arm';
+		'RightUpperArm';
+		'RightLowerArm';
+		'RightHand';
+		'Right Arm';
+	};
+	LowerBody = {
+		'LowerTorso';
+		'RightUpperLeg';
+		'RightLowerLeg';
+		'RightFoot';
+		'Right Leg';
+		'LeftUpperLeg';
+		'LeftLowerLeg';
+		'LeftFoot';
+		'Left Leg';
+	};
+	Waist = {
+		'LowerTorso';
+		'RightUpperLeg';
+		'LeftUpperLeg';
+	};
+	Arms = {
+		'LeftUpperArm';
+		'LeftLowerArm';
+		'LeftHand';
+		'Left Arm';
+		'RightUpperArm';
+		'RightLowerArm';
+		'RightHand';
+		'Right Arm';
+	};
+	Legs = {
+		'RightUpperLeg';
+		'RightLowerLeg';
+		'RightFoot';
+		'Right Leg';
+		'LeftUpperLeg';
+		'LeftLowerLeg';
+		'LeftFoot';
+		'Left Leg';
+	};
+	All = {
+		'Head';
+		'LowerTorso';
+		'UpperTorso';
+		'Torso';
+		'RightUpperLeg';
+		'RightLowerLeg';
+		'RightFoot';
+		'Right Leg';
+		'LeftUpperLeg';
+		'LeftLowerLeg';
+		'LeftFoot';
+		'Left Leg';
+		'LeftUpperArm';
+		'LeftLowerArm';
+		'LeftHand';
+		'Left Arm';
+		'RightUpperArm';
+		'RightLowerArm';
+		'RightHand';
+		'Right Arm';
+	};
+};
